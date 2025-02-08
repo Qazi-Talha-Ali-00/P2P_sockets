3b68b542fe6fb8d9c9ef33a27f7a57f1bdbb506b
Qazi-Talha-Ali-00 <b22cs087@iitj.ac.in>
2025-02-08 19:43:32 +0530

    added the logic to delete a peer

--- seed_own.cpp
+++ seed_own.cpp
@@ -9,22 +9,48 @@
 using namespace std;
 map<string, int> peer_list;
 mutex m;
+bool isalive(string ip, string port){
+    return true;
+}
+void handle_dead_node(string& message){
+    istringstream iss(message);
+    string token;
+    vector<string> tokens;
+
+    // Split the message by ':'
+    while (getline(iss, token, ':')) {
+        tokens.push_back(token);
+    }
+    m.lock();
+    if(peer_list.count(tokens[1]) == 0){
+        m.unlock();
+        return;
+     }
+    m.unlock();
+    // now we have to delete this node from the p2p network
+    // first check if it is actually dead.
+    // send the ip and port
+    isalive(tokens[1],tokens[2]);
+}
 void handle_client(int client_socket_fd, sockaddr_in client_addr){
-        // create a new socket for this client
-     char client_ip[INET_ADDRSTRLEN];
+    // get the client ip and port number from the client_add struct
+    char client_ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
     int client_port = ntohs(client_addr.sin_port);
-    m.lock();
-    peer_list[client_ip] = client_port;
-    m.unlock();
-
+    // create a peer_list to send to this peer
     string peer_list_msg = "";
     m.lock();
     for (auto& peer : peer_list) {
         peer_list_msg += peer.first + " " + to_string(peer.second) + ",";
     }
     m.unlock();
+    // first send this peer list to the peer
     send(client_socket_fd, peer_list_msg.c_str(), peer_list_msg.length(), 0);
+    // now push this clien into our own peer list.
+     m.lock();
+    peer_list[client_ip] = client_port;
+    m.unlock();
+    // write in the log file that a new peer has been registered.
     m.lock();
     ofstream fout("log.txt");
     if(!fout){
@@ -33,7 +59,27 @@ void handle_client(int client_socket_fd, sockaddr_in client_addr){
     }
     fout<<"New peer registered "<<client_ip<<": "<<client_port<<endl;
     fout.close();
-    m.unlock();   
+    m.unlock();  
+     
+    char buffer[1024];
+
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));  // Clear buffer
+        int bytes_received = recv(client_socket_fd, buffer, sizeof(buffer) - 1, 0);
+
+        if (bytes_received <= 0) {
+            continue;
+            
+        }
+
+        buffer[bytes_received] = '\0';  // Null-terminate
+        string message(buffer);
+
+        // Check if it's a "Dead Node" message
+        if (message.find("Dead Node:") == 0) {
+            handle_dead_node(message);
+        }
+    }
 }
 int main(int argc ,char *argv[])
 {
